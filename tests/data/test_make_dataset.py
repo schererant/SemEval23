3d12c2740501779c20dee1c6bb5df9c8997739cd
schererant <67601986+schererant@users.noreply.github.com>
2022-12-14 16:41:36 +0100

    Make tests worse

--- tests/data/test_make_dataset.py
+++ tests/data/test_make_dataset.py
@@ -2,42 +2,34 @@ import pytest
 import os
 from src.data.make_dataset import load_labels_from_tsv, load_arguments_from_tsv, load_values_from_json, load_json_file
 
+script_dir = os.getcwd()  # <-- absolute dir the script is in
+rel_path = "data/raw/"
+data_dir = os.path.join(script_dir, rel_path)
 
-@pytest.fixture
-def filepath_json():
-    curr_dir = os.getcwd()
-    data_dir = os.path.join(curr_dir, '/data/raw/')
-    filepath = str(data_dir + 'value-categories.json')
-    return filepath
-
-
-@pytest.fixture
-def filepath_labels():
-    curr_dir = os.getcwd()
-    data_dir = os.path.join(curr_dir, '/data/raw/')
-    return os.path.join(data_dir, 'labels-training.tsv')
+filepath_json = os.path.join(data_dir, 'value-categories.json')
+filepath_labels = os.path.join(data_dir, 'labels-training.tsv')
+filepath_data = os.path.join(data_dir, 'arguments-training.tsv')
 
 
-@pytest.fixture
-def filepath_data():
-    curr_dir = os.getcwd()
-    data_dir = os.path.join(curr_dir, '/data/raw/')
-    return os.path.join(data_dir, 'arguments-training.tsv')
+def pytest_configure():
+    pytest.data_dir = data_dir
+    pytest.filepath_json = filepath_json
+    pytest.filepath_labels = filepath_labels
+    pytest.filepath_data = filepath_data
 
+
+#%%
 
 # Test hard coded path
 def test_path_exists():
-    assert os.path.isdir('/data/')
+    script_dir = os.getcwd()  # <-- absolute dir the script is in
+    rel_path = "../data/raw/"
+    data_dir = os.path.join(script_dir, rel_path)
+    assert os.path.isdir(data_dir)
 
 
 # TODO: use fixtures
 def test_load_labels_from_tsv():
-    filepath_json = '/data/raw/value-categories.json'
-    filepath_labels = '/data/raw/labels-training.tsv'
-    # curr_dir = os.getcwd()
-    # data_dir = os.path.join(curr_dir, '/data/raw/')
-    # # filepath_json = os.path.join(data_dir, 'value-categories.json')
-    # filepath_json = data_dir + 'value-categories.json'
     json_values = load_json_file(filepath_json)
     load_labels_from_tsv(filepath_labels, list(json_values.keys()))
     assert True
