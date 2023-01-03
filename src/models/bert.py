1b620dbc9d0edb4c5413abff2531f0adc5070fe5
mstuffer <mstuffer@soundhound.com>
2023-01-03 19:49:58 +0100

    move prediction methods to new file

--- src/models/bert.py
+++ src/models/bert.py
@@ -1,6 +1,5 @@
 import torch
 import numpy as np
-from sklearn.metrics import f1_score
 from transformers import (AutoTokenizer, AutoModelForSequenceClassification,
                           PreTrainedModel, DistilBertModel, BertModel, BertForSequenceClassification,
                           TrainingArguments, Trainer)
@@ -11,43 +10,6 @@ from datasets import (Dataset, DatasetDict, load_dataset)
 The folllwoing code is taken from https://github.com/webis-de/acl22-identifying-the-human-values-behind-arguments/blob/main/src/python/components/models/bert.py
 
 """
-def accuracy_thresh(y_pred, y_true, thresh=0.5, sigmoid=True):
-    """Compute accuracy of predictions"""
-    y_pred = torch.from_numpy(y_pred)
-    y_true = torch.from_numpy(y_true)
-    if sigmoid:
-        y_pred = y_pred.sigmoid()
-
-    return ((y_pred > thresh) == y_true.bool()).float().mean().item()
-
-
-def f1_score_per_label(y_pred, y_true, value_classes, thresh=0.5, sigmoid=True):
-    """Compute label-wise and averaged F1-scores"""
-    y_pred = torch.from_numpy(y_pred)
-    y_true = torch.from_numpy(y_true)
-    if sigmoid:
-        y_pred = y_pred.sigmoid()
-
-    y_true = y_true.bool().numpy()
-    y_pred = (y_pred > thresh).numpy()
-
-    f1_scores = {}
-    for i, v in enumerate(value_classes):
-        f1_scores[v] = round(f1_score(y_true[:, i], y_pred[:, i], zero_division=0), 2)
-
-    f1_scores['avg-f1-score'] = round(np.mean(list(f1_scores.values())), 2)
-
-    return f1_scores
-
-
-def compute_metrics(eval_pred, value_classes):
-    """Custom metric calculation function for MultiLabelTrainer"""
-    predictions, labels = eval_pred
-    f1scores = f1_score_per_label(predictions, labels, value_classes)
-    return {'accuracy_thresh': accuracy_thresh(predictions, labels), 'f1-score': f1scores,
-            'marco-avg-f1score': f1scores['avg-f1-score']}
-
-
 class MultiLabelTrainer(Trainer):
     """
         A transformers `Trainer` with custom loss computation
