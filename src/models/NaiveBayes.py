69ef979a496aeb4aa1276eaa49a33b872fc258fe
Natasha <n.t.j.v.d.berg@student.tue.nl>
2023-01-04 16:00:29 +0100

    changed naive bayes to class structure

--- src/models/NaiveBayes.py
+++ src/models/NaiveBayes.py
@@ -5,42 +5,70 @@ from sklearn.pipeline import Pipeline
 from sklearn.multiclass import OneVsRestClassifier
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.metrics import accuracy_score
+import numpy as np
+import pandas as pd
 
-def df_dataset(filepath_arguments, filepath_labelorder, filepath_labels):
-    df_arguments = md.load_arguments_from_tsv(filepath_arguments)
+class NBModel():
+    def __init__(self):
+        return
+
+    def init_pipeline(self):
+        stop_words = list(stopwords.words('english'))
+        return Pipeline([
+            ('tfidf', TfidfVectorizer(stop_words=stop_words)),
+            ('clf', OneVsRestClassifier(MultinomialNB(
+                fit_prior=True, class_prior=None))),
+        ])
+
+    def train(self, train_frame, label_columns):
+        # Define a pipeline combining a text feature extractor with multi lable classifier
+        self.label_columns = label_columns
+        self.NB_pipelines = {label_col: self.init_pipeline() for label_col in label_columns}
+
+        for label_col in label_columns:
+            self.NB_pipelines[label_col].fit(train_frame['Premise'], train_frame[label_col])
+
+    def predict(self, dataframe):
+        preds = {}
+
+        for col in self.label_columns:
+            preds[col] = self.NB_pipelines[col].predict(dataframe['Premise'])
+
+        pred_df = pd.DataFrame(preds)
+
+        return pred_df
+
+    def evaluate(self, data_frame):
+        pred_df = self.predict(data_frame)
+        scores = {col: accuracy_score(pred_df[col], data_frame[col]) for col in self.label_columns}
+
+        print(f"Accuracy score: {scores}")
+        return scores
+
+
+if __name__ == "__main__":
+    def df_dataset(filepath_arguments, filepath_labelorder, filepath_labels):
+        df_arguments = md.load_arguments_from_tsv(filepath_arguments)
+        label_order = md.load_json_file(filepath_labelorder)
+        df_labels = md.load_labels_from_tsv(filepath_labels, list(label_order.keys()))
+        df_merged = md.merge_arg_label(df_arguments, df_labels)
+        return df_merged
+
+
+    filepath_arguments_train = "../../data/processed/arguments-training_unify_stance.tsv"
+    filepath_labelorder = "../../data/raw/value-categories.json"
+    filepath_labels_train = "../../data/raw/labels-training.tsv"
+
+    filepath_arguments_val = "../../data/processed/arguments-validation_unify_stance.tsv"
+    filepath_labels_val = "../../data/raw/labels-validation.tsv"
+
+    df_merged_train = df_dataset(filepath_arguments_train, filepath_labelorder, filepath_labels_train)
+    df_merged_validation = df_dataset(filepath_arguments_val, filepath_labelorder, filepath_labels_val)
+
     label_order = md.load_json_file(filepath_labelorder)
-    df_labels = md.load_labels_from_tsv(filepath_labels, list(label_order.keys()))
-    df_merged = md.merge_arg_label(df_arguments, df_labels)
-    return df_merged
+    label_columns = label_order.keys()
 
-filepath_arguments_train = "../../data/processed/arguments-training_unify_stance.tsv"
-filepath_labelorder = "../../data/raw/value-categories.json"
-filepath_labels_train = "../../data/raw/labels-training.tsv"
-
-
-filepath_arguments_val = "../../data/processed/arguments-validation_unify_stance.tsv"
-filepath_labels_val = "../../data/raw/labels-validation.tsv"
-
-df_merged_train = df_dataset(filepath_arguments_train, filepath_labelorder, filepath_labels_train)
-df_merged_validation = df_dataset(filepath_arguments_val, filepath_labelorder, filepath_labels_val)
-
-stop_words = list(stopwords.words('english'))
-
-# Define a pipeline combining a text feature extractor with multi lable classifier
-NB_pipeline = Pipeline([
-                ('tfidf', TfidfVectorizer(stop_words = stop_words)),
-                ('clf', OneVsRestClassifier(MultinomialNB(
-                    fit_prior=True, class_prior=None))),
-            ])
-
-
-def train_accuracy(labelorder, df_merged_train, df_merged_validation):
-    for label in list(labelorder.keys()):
-        print('... Processing {}'.format(label))
-        NB_pipeline.fit(df_merged_train["Premise"], df_merged_train[label])
-        prediction = NB_pipeline.predict(df_merged_validation["Premise"])
-        print('Accuracy is {}'.format(accuracy_score(df_merged_validation[label], prediction)))
-
-
-label_order = md.load_json_file(filepath_labelorder)
-train_accuracy(label_order, df_merged_train, df_merged_validation)
+    nb_model = NBModel()
+    nb_model.train(df_merged_train, label_columns)
+    print(nb_model.predict(df_merged_validation))
+    nb_model.evaluate(df_merged_validation)
