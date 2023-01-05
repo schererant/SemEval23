fee52ceab77a7cc5cc3fdeeb7de5332f38c00bdb
mstuffer <mstuffer@soundhound.com>
2023-01-05 17:50:38 +0100

    add config

--- src/models/train_model.py
+++ src/models/train_model.py
@@ -1,4 +1,5 @@
 from src.models.bert import *
+from src.models.NaiveBayes import *
 import src.models as models
 import os
 
@@ -26,6 +27,8 @@ def get_model(config: dict)->ModelInterface:
     model_name = config['model']['name'].lower()
     if model_name == 'bert':
         return BertModel(config)
+    if model_name == 'naive_bayes':
+        return NBModel(config)
     # add new models here
     # elif
     #   ...
